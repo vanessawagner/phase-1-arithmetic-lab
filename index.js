a3378b873efdce934c67dc1e332a9fec21176e8a
Vanessa Wagner <vmwagner@gmail.com>
2022-05-17 13:57:58 -0500

    commit tree 36229e11c85360ff4bb83fe6ee91bf21eee90553

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.round(Math.random())
+
+let num3 = 8
+let num4 = 2
+let mod = num3 / num4
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+console.log(max)
+
